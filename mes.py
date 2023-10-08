e7a87f9b0308bea3d7de0e213101a50cac40d4da
zaeso <140312201+zaeso@users.noreply.github.com>
2023-10-08 13:17:28 +0300

    Update mes.py

--- mes.py
+++ mes.py
@@ -1,6 +1,8 @@
 from settings import settings
 import discord
 from discord.ext import commands
+import os , random
+import requests
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -20,14 +22,49 @@ async def heh(ctx, count_heh = 5):
     await ctx.send("he" * count_heh)
 
 @bot.command()
-async def repeat(ctx, times: int, content='repeating...'):
-    """Repeats a message multiple times."""
-    for i in range(times):
-       await ctx.send(content)
-
-@bot.command()
 async def add(ctx, left: int, right: int):
     """Adds two numbers together."""
     await ctx.send(left + right)
-    
-bot.run(settings[""])
+
+@bot.command()
+async def repeat(ctx, times: int, content='repeating...'):
+    """Repeats a message multiple times."""
+    for i in range(times):
+        await ctx.send(content)
+ 
+@bot.command()
+async def mem(ctx):
+    img_name= random.choice(os.listdir("imanges"))
+    with open(f'imanges/{img_name}', 'rb') as f:
+        # В переменную кладем файл, который преобразуется в файл библиотеки Discord!
+        picture = discord.File(f)
+   # Можем передавать файл как параметр!
+    await ctx.send(file=picture)       
+
+def get_dog_image_url():    
+    url = 'https://random.dog/woof.json'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+
+@bot.command('dog')
+async def dog(ctx):
+    image_url = get_dog_image_url()
+    await ctx.send(image_url)
+
+def get_fox_image_url():    
+    url = 'https://randomfox.ca/floof/'
+    res = requests.get(url)
+    data = res.json()
+    return data['image']
+
+
+@bot.command('fox')
+async def fox(ctx):
+    '''По команде duck вызывает функцию get_duck_image_url'''
+    image_url = get_fox_image_url()
+    await ctx.send(image_url)
+
+
+bot.run(settings["TOKEN"])
